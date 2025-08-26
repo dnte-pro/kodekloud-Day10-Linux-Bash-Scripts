c60068b4af19c5914d6f193426aa5394677a6b5a
Kiprono Yegon <78739881+dnte-pro@users.noreply.github.com>
2025-08-26 23:33:15 +0300

    Delete blog_backup.sh

--- blog_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a zip archive of the official directory
-zip -r /backup/xfusioncorp_official.zip /var/www/html/official
-
-# Copy the archive to the backup server
-scp /backup/xfusioncorp_official.zip clint@stbkp01:/backup/
